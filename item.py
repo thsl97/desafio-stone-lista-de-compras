dde30edb5a143a8a5dc99a08c408a3be09ce8033
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2021-01-21 20:16:46 -0300

    Inicializa projeto, cria classe de Item, funcao de lista de compras para somar itens e teste de unidade para verificar a soma dos itens de uma lista

---
+++ item.py
@@ -0,0 +1,4 @@
+class Item:
+
+    def __init__(self, valor):
+        self.valor = valor
---
+++ lista_compras.py
@@ -0,0 +1,5 @@
+def somar_itens(lista_itens):
+    total = 0
+    for item in lista_itens:
+        total += item.valor
+    return total
