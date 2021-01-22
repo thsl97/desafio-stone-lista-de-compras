72f04eb22f3c187d0f2b2eeebd35f5f30224d9ba
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2021-01-21 22:10:09 -0300

    Adiciona docstring a main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from shopping_list import ShoppingList
 
 
 def main():
+    """
+    Change variables item_list and email_list for desired input values
+    :return:
+    """
     item_list = [Item(quantity=1, price=17)]
     email_list = ['user1@example.com', 'user2@example.com', 'user3@example.com', 'user4@example.com',
                   'user5@example.com']
